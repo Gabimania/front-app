e86a91a72703eca2dfcd706526f67b2c5c3544f7
Gabriel Enríquez <gabrielenlo9@gmail.com>
2023-11-07 23:03:46 +0100

    Developing the front of the app

---
+++ src/app/contact-home/contact-home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactsService } from '../contacts.service';
+
+@Component({
+  selector: 'app-contact-home',
+  templateUrl: './contact-home.component.html',
+  styleUrls: ['./contact-home.component.css']
+})
+export class ContactHomeComponent implements OnInit {
+  contacts: any = [];
+  constructor(private contactsService: ContactsService){}
+
+  ngOnInit(): void {
+   this.contactsService.getContacts().subscribe(data =>{
+    this.contacts = data; 
+  })
+  }
+
+  displayedColumns: string[] = ['id', 'name', 'username', 'secondusername', 'phone', 'email'];
+  
+  
+}
---
+++ src/app/contacts.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+
+  constructor(private http: HttpClient) { }
+
+  getContacts(): Observable<any>{
+    const url = 'http://localhost:30030/contacts/getAll';
+    const headers = new HttpHeaders();
+    return this.http.get<any>(url,{headers});
+  }
+}
